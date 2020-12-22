958b459dc033c030e9f54507c7b68215da25b68f
dimasprado <73427703+dimasprado@users.noreply.github.com>
2020-12-22 11:24:37 +0500

    Add files via upload

---
+++ Library/Stack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Library
+{
+    public class Stack<T>
+    {
+        private List<T> Container;
+        public Stack()
+        {
+            Container = new List<T>();
+        }
+        /// <summary>
+        /// Количество элементов в стеке
+        /// </summary>
+        public int Count {
+            get {
+                return Container.Count;
+            } 
+        }
+        /// <summary>
+        /// Получаем значение последнего элемента в стеке
+        /// </summary>
+        /// <returns>ПОследний элемент в стеке</returns>
+        public T Check()
+        {
+            return Count != 0 ? Container[0] : throw new Exception("Нет элементов в стеке");
+        }
+        /// <summary>
+        /// Удаляет последний элемент в стеке и возвращает его
+        /// </summary>
+        /// <returns>последний элемент в стеке</returns>
+        public T Pop()
+        {
+            var tmp = Check();
+            Container.Remove(tmp);
+            return tmp;
+        }
+        /// <summary>
+        /// Добавление элемента в стек
+        /// </summary>
+        /// <param name="item">элемент который добавляем</param>
+        public void Push(T item)
+        {
+            Container.Insert(0, item);
+        }
+        /// <summary>
+        /// Проверка на наличие элемента в стеке
+        /// </summary>
+        /// <param name="item">непосредственно элемент</param>
+        /// <returns>true если есть такой элемент в стеке</returns>
+        public bool Contains(T item)
+        {
+            return Container.Contains(item);
+        }
+    }
+}
---
+++ lab1_5/Program.cs
@@ -0,0 +1,77 @@
+﻿using Library;
+using System;
+using System.Drawing;
+
+namespace lab1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Непосредственно лабиринт
+            var lab = new bool[][]
+            {
+                new bool[]{true, true, true, false},
+                new bool[]{ false, false, true, false},
+                new bool[]{ true, true, true, false},
+                new bool[]{ true, false, false, false},
+                new bool[]{ true, true, true, true},
+            };
+            var stack = new Stack<Point>();
+            // Начало лабиринта
+            stack.Push(new Point(0, 0));
+            FindWay(stack, lab);
+            while(stack.Count != 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+        /// <summary>
+        /// Функция для прохода по лабиринту
+        /// </summary>
+        /// <param name="stack">Стек для прохода по лабиринту</param>
+        /// <param name="lab">Лабиринт</param>
+        /// <returns>true если выход найден</returns>
+        static bool FindWay(Stack<Point> stack,bool[][] lab)
+        {
+            var last = stack.Check();
+            // Если дошли до конца возвращаем true
+             if (last.X == lab[0].Length - 1 && last.Y == lab.Length - 1)
+            {
+                return true;
+            }
+            return Move(stack, lab, new Point(last.X + 1, last.Y)) ||
+                Move(stack, lab, new Point(last.X - 1, last.Y)) ||
+                Move(stack, lab, new Point(last.X, last.Y + 1)) ||
+                Move(stack, lab, new Point(last.X, last.Y - 1));
+        }
+        /// <summary>
+        /// Движение по лабиринту
+        /// </summary>
+        /// <param name="stack">Стек для движения по лабиринту</param>
+        /// <param name="lab">Лабиринт</param>
+        /// <param name="to">Точка в которую мы идем</param>
+        /// <returns>true если мы можем из этой точки дойти до конца лабиринта</returns>
+        static bool Move(Stack<Point> stack, bool[][] lab, Point to)
+        {
+            if (to.X < 0 || to.Y < 0)
+            {
+                return false;
+            }
+            // Не уходим за границы и не ходим по уже пройденным местам
+            if (lab[0].Length > to.X && lab.Length > to.Y && lab[to.Y][to.X] == true && !stack.Contains(to))
+            {
+                stack.Push(to);
+                if (FindWay(stack, lab))
+                {
+                    return true;
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+            return false;
+        }
+    }
+}
